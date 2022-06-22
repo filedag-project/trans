72593746188d54a5511b4d4e02a98f473e6590fa
beeleelee <beeleelee@outlook.com>
2022-06-22 12:42:06 +0800

    feat:config vbuf

--- pool.go
+++ pool.go
@@ -2,7 +2,11 @@ package trans
 
 import "sync"
 
-const v_buf_size = 4<<20 + 256
+const VBUF_4M = 4 << 20
+const VBUF_1M = 1 << 20
+
+var v_buf_size = VBUF_1M + 256
+
 const short_buf_size = 4 << 10
 
 var (
@@ -31,8 +35,12 @@ type buffer []byte
 func (v *buffer) size(size int) {
 	if cap(*v) < size {
 		old := *v
-		*v = make([]byte, size, 2*size)
+		*v = make([]byte, size, size+short_buf_size)
 		copy(*v, old)
 	}
 	*v = (*v)[:size]
+}
+
+func SetVBuf4M() {
+	v_buf_size = VBUF_4M + 256
 }
